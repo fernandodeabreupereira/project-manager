31d24feca26fe30355102d1bcfa5fe423383514a
Fernando de Abreu Pereira <fernandodeabreupereira@gmail.com>
2020-12-29 14:53:01 -0300

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+
+app.listen(3333);
